ab0374602693553d2eefd1fa0874b59819242fc5
Daniel Jorge Manzano <djorgemanzano@gmail.com>
2026-03-18 09:51:15 -0300

    aula 6, intro a js

---
+++ aula06/introducao.js
@@ -0,0 +1,10 @@
+// 'olá mundo' em js
+
+function saudacao(nome) {
+    return `Olá, ${nome}!`;
+}
+
+let nome = "mundo";
+console.log(saudacao(nome));
+
+// a aula era maior e tinha bastante coisa sobre js, mas como não gosto dessa "linguagem", prefiro ficar com só isso
